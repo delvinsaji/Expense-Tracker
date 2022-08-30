ec22e291f972ad4be6c5d8bb8974bc1b05801527
delvinsaji <91379593+delvinsaji@users.noreply.github.com>
2022-08-30 23:00:47 +0530

    :

--- App.js
+++ App.js
@@ -4,55 +4,86 @@ import Recent from "./Screens/Recent";
 import All from "./Screens/All";
 import { Ionicons, AntDesign } from "react-native-vector-icons";
 import { GlobalStyles } from "./Styles";
+import { useState } from "react";
+import { Modal, View, Pressable } from "react-native";
 
 const Bottom = createBottomTabNavigator();
 
 export default function App() {
+  const [data, setData] = useState([]);
+  const [modal, setModal] = useState(false);
   return (
-    <NavigationContainer>
-      <Bottom.Navigator
-        screenOptions={{
-          headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
-          headerTintColor: "white",
-          tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
-          tabBarActiveTintColor: GlobalStyles.colors.accent500,
-        }}
-      >
-        <Bottom.Screen
-          name="Recent Expenses"
-          component={Recent}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="hourglass" color={color} size={size} />
-            ),
-            headerRight: () => (
-              <AntDesign
-                name="plus"
-                color={GlobalStyles.colors.accent500}
-                size={20}
-                style={{ marginRight: 10 }}
-              />
-            ),
+    <View style={{ flex: 1 }}>
+      <Modal visible={modal} animationType="slide">
+        <View
+          style={{ backgroundColor: GlobalStyles.colors.primary400, flex: 1 }}
+        ></View>
+      </Modal>
+      <NavigationContainer>
+        <Bottom.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+            headerTintColor: "white",
+            tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+            tabBarActiveTintColor: GlobalStyles.colors.accent500,
           }}
-        />
-        <Bottom.Screen
-          name="All Expenses"
-          component={All}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="calendar" color={color} size={size} />
-            ),
-            headerRight: () => (
-              <AntDesign
-                name="plus"
-                color={GlobalStyles.colors.accent500}
-                size={20}
-                style={{ marginRight: 10 }}
-              />
-            ),
-          }}
-        />
-      </Bottom.Navigator>
-    </NavigationContainer>
+        >
+          <Bottom.Screen
+            name="Recent Expenses"
+            component={Recent}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="hourglass" color={color} size={size} />
+              ),
+              headerRight: () => {
+                return (
+                  <View>
+                    <Pressable
+                      onPress={() => {
+                        setModal(true);
+                      }}
+                    >
+                      <AntDesign
+                        name="plus"
+                        color={GlobalStyles.colors.accent500}
+                        size={20}
+                        style={{ marginRight: 10 }}
+                      />
+                    </Pressable>
+                  </View>
+                );
+              },
+            }}
+          />
+          <Bottom.Screen
+            name="All Expenses"
+            component={All}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="calendar" color={color} size={size} />
+              ),
+              headerRight: () => {
+                return (
+                  <View>
+                    <Pressable
+                      onPress={() => {
+                        setModal(true);
+                      }}
+                    >
+                      <AntDesign
+                        name="plus"
+                        color={GlobalStyles.colors.accent500}
+                        size={20}
+                        style={{ marginRight: 10 }}
+                      />
+                    </Pressable>
+                  </View>
+                );
+              },
+            }}
+          />
+        </Bottom.Navigator>
+      </NavigationContainer>
+    </View>
   );
 }
